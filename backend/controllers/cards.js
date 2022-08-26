3e713a1ddf21a9ba93c2ed756687931db1794b14
Vladislav Evdokimov <gangrena14@yandex.ru>
2022-08-26 22:38:00 +0300

    fix: убрать лишний блок catch

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -13,27 +13,28 @@ module.exports.createCard = (req, res, next) => {
     })
     .catch((error) => {
       if (error.name === 'ValidationError') {
-        throw new BadRequest('Некорректные данные');
-      } next(error);
-    })
-    .catch(next);
+        next(new BadRequest('Некорректные данные'));
+      } else {
+        next(error);
+      }
+    });
 };
 module.exports.getCard = (req, res, next) => {
   Card.find({})
     .then((data) => {
       res.status(errorStatus.SUCCESSFUL_REQUEST).send(data);
     })
-    .catch((error) => {
-      if (error.name === 'CastError') {
-        throw new NotFoundError('Карточка не найдена');
-      } next(error);
-    })
+    // .catch((error) => {
+    //   if (error.name === 'CastError') {
+    //     throw new NotFoundError('Карточка не найдена');
+    //   } next(error);
+    // })
     .catch(next);
 };
 module.exports.deleteCard = (req, res, next) => {
   const cardId = req.params.id;
   const id = req.user._id;
-  Card.findByIdAndRemove(cardId)
+  Card.findById(cardId)
     .then((card) => {
       if (!card) {
         throw new NotFoundError(`Карточка  с указанным id: ${cardId} не найдена`);
@@ -47,7 +48,7 @@ module.exports.deleteCard = (req, res, next) => {
           })
           .catch((error) => {
             if (error.name === 'CastError') {
-              throw new BadRequest(`Карточка с id:${cardId} не найдена`);
+              next(new BadRequest(`Карточка с id:${cardId} не найдена`));
             }
             next(error);
           })
@@ -72,10 +73,10 @@ module.exports.likeCard = (req, res, next) => {
     .catch((error) => {
       if (error.name === 'CastError') {
         throw new BadRequest('Карточка не найдена');
+      } else {
+        next(error);
       }
-      next(error);
-    })
-    .catch(next);
+    });
 };
 module.exports.disLikeCard = (req, res, next) => {
   const cardId = req.params.id;
@@ -92,9 +93,9 @@ module.exports.disLikeCard = (req, res, next) => {
     })
     .catch((error) => {
       if (error.name === 'CastError') {
-        throw new BadRequest('Карточка не найдена');
+        next(new BadRequest('Карточка не найдена'));
+      } else {
+        next(error);
       }
-      next(error);
-    })
-    .catch(next);
+    });
 };

183f871108c107358cca54c06bdc5aef5fb3c6af
Vladislav Evdokimov <gangrena14@yandex.ru>
2022-08-27 00:03:17 +0300

    fix: пофиксить порядок роутов

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -13,19 +13,23 @@ const {
 // usersRouter.post('/users', createUser);
 
 usersRouter.get('/users', getUsers);
+
+usersRouter.get('/users/me', getCurrentUserInfo);
+
 usersRouter.get('/users/:id', celebrate({
   params: Joi.object().keys({
     id: Joi.string().required().hex().min(24)
       .max(24),
   }),
 }), getUserById);
-usersRouter.get('/users/me', getCurrentUserInfo);
+
 usersRouter.patch('/users/me', celebrate({
   body: Joi.object().keys({
     name: Joi.string()./* required(). */min(2).max(30),
     about: Joi.string()./* required(). */min(2).max(30),
   }),
 }), updateUserInfo);
+
 usersRouter.patch('/users/me/avatar', celebrate({
   body: Joi.object().keys({
     avatar: Joi.string().regex(/^(https?:\/\/)?([\da-z.-]+).([a-z.]{2,6})([/\w.-]*)*\/?$/),
